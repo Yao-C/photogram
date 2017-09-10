a1941e5a05cba4f96b13eab89887efe64c5943ac
Yao Cheng <chengyao19920131@163.com>
2017-09-10 13:57:09 +0000

    Add Users and Photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  resources :photos
+  devise_for :users
+  root 'photos#index'
+  get 'contact' => 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
